82ef188b2c6f2a0816ee1a9647b3915a245ab751
MatheusFranceschini <masuzena@gmail.com>
2024-09-19 19:50:47 -0300

    Initial Commit

---
+++ Whitehouse Petitions/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Whitehouse Petitions
+//
+//  Created by Matheus Franceschini on 19/09/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
